350e3922ed6ff7ace9b8b2250a109e869312503f
Visay Vang <vsvang@gmail.com>
2023-01-03 12:09:38 -0500

    1/3/23, change grid layout

--- js/app.js
+++ js/app.js
@@ -11,17 +11,21 @@ form.addEventListener("submit", (e) => {
   //call function
   fetchAPI();
 });
+//fetching data
 async function fetchAPI() {
   const url = `https://api.edamam.com/search?q=${searchInput.value}&app_id=${appID}&app_key=${appKeys}&to=20`;
   const response = await fetch(url);
   const data = await response.json();
+  //calling function and passing data into function
   renderHtml(data.hits);
   console.log(data);
 }
 
 function renderHtml(results) {
   let render = "";
+  //mapping through all data and displaying it in HTML
   results.map((result) => {
+    //render data into HTML with template string
     render += `
      <div class="result-recipe">
             <img src="${result.recipe.image}" alt="" />
@@ -30,8 +34,6 @@ function renderHtml(results) {
               <p class="calories">Calories: ${result.recipe.calories.toFixed(
                 2
               )}</p>
-              
-
                 <a href="${
                   result.recipe.url
                 }" target="_black"><button class="btn">Recipe</button></a>
